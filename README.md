3aba5103557eb354bb37701725aa2ee420d66320
Juan Alves <juanfalvesm@gmail.com>
2024-11-20 18:26:25 -0300

    base

---
+++ README.md
@@ -0,0 +1,9 @@
+# Integrantes
+Ahmad Jaafar
+RM: 97927
+
+Augusto Ferreira Rogel de Souza
+RM: 5557709
+
+Juan Francisco Alves Muradas
+RM: 555541
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import styled, { createGlobalStyle } from "styled-components"
+import EstilosGlobais from './components/Global'
+
+
+function App() {
+
+  return (
+    <>
+      <EstilosGlobais />
+    </>
+  )
+}
+
+export default App
